d4ba1aef1c002f225f8d7021eb26c8fbd66ab773
princess.balogun <princess.balogun@ashesi.edu.gh>
2024-12-17 20:37:26 +0000

    changed database to fit the server connection

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,9 +1,9 @@
 <?php 
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','scholarshipdb');
+define('DB_USER','princess.balogun');
+define('DB_PASS','ZyxB09$$');
+define('DB_NAME','webtech_fall2024_princess_balogun');
 // Establish database connection.
 try
 {
